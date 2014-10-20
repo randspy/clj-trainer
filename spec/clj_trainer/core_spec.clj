52522dccdd73f95cd988ad0c00fa5486067ad608
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-10-20 20:25:41 +0200

    First commit

---
+++ spec/clj_trainer/core_spec.clj
@@ -0,0 +1,7 @@
+(ns clj-trainer.core-spec
+  (:require [speclj.core :refer :all]
+            [clj-trainer.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/clj_trainer/core.clj
@@ -0,0 +1,1 @@
+(ns clj-trainer.core)
